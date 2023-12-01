260cde151e2458a095967cedc7925d166c624945
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-12-01 16:57:20 +0800

    added test and backend functions

--- backend/contracts/TicketCreation.sol
+++ backend/contracts/TicketCreation.sol
@@ -6,13 +6,42 @@ import "@openzeppelin/contracts/access/Ownable.sol"; // Correct import statement
 
 contract TicketCreation is ERC721, Ownable {
     //address public owner;
-
     /*
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         owner = msg.sender;
     }
     */
+    uint256 public totalConcerts;
+
+    struct Concert {
+        uint256 id;
+        string name;
+        string date;
+        string time;
+        string location;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+    }
+    mapping(uint256 => Concert) concerts;
 
     constructor(string memory _name, string memory _symbol, address initialOwner) ERC721(_name, _symbol)
         Ownable(initialOwner) {}
+
+    function list(
+        string memory _name,
+        string memory _date,
+        string memory _time,
+        string memory _location,
+        uint256 _cost,
+        uint256 _maxTickets
+    ) public onlyOwner {
+        totalConcerts++;
+        concerts[totalConcerts] = Concert(totalConcerts, _name, _date, _time, _location, _cost, _maxTickets, _maxTickets);
+    }
+
+    function getConcert(uint256 _id) public view returns (Concert memory) {
+        return concerts[_id];
+    }
+    
 }
--- backend/test/TicketCreation.test.js
+++ backend/test/TicketCreation.test.js
@@ -3,6 +3,14 @@ const { expect } = require("chai");
 const NAME = "TicketCreation";
 const SYMBOL = "TC";
 
+//Create a test concert
+const CONCERT_NAME = "Aespa World Tour Seoul"
+const CONCERT_DATE = "Apr 27"
+const CONCERT_TIME = "17:00AM KST"
+const CONCERT_LOCATION = "Seoul, South Korea"
+const CONCERT_COST = ethers.utils.parseUnits('1', 'ether')
+const CONCERT_MAX_TICKETS = 100
+
 describe("Ticket-Creation", () => {
   let tc; 
   let deployer;
@@ -13,6 +21,16 @@ describe("Ticket-Creation", () => {
 
     const TicketCreation = await ethers.getContractFactory("TicketCreation");
     tc = await TicketCreation.deploy(NAME, SYMBOL, deployer.address);
+
+    const txn = await tc.connect(deployer).list(
+        CONCERT_NAME,
+        CONCERT_DATE,
+        CONCERT_TIME,
+        CONCERT_LOCATION,
+        CONCERT_COST,
+        CONCERT_MAX_TICKETS
+    );
+    await txn.wait();
   });
 
   describe("try deployment", () => {
@@ -26,6 +44,24 @@ describe("Ticket-Creation", () => {
     it("Sets the owner address", async () => {
         expect(await tc.owner()).to.equal(deployer.address);
       });
+  });
+
+  describe("try Concerts", () => {
+    it('Returns Concert information', async () => {
+        const concert = await tc.getConcert(1)
+        expect(concert.id).to.be.equal(1)
+        expect(concert.name).to.be.equal(CONCERT_NAME)
+        expect(concert.cost).to.be.equal(CONCERT_COST)
+        expect(concert.tickets).to.be.equal(CONCERT_MAX_TICKETS)
+        expect(concert.date).to.be.equal(CONCERT_DATE)
+        expect(concert.time).to.be.equal(CONCERT_TIME)
+        expect(concert.location).to.be.equal(CONCERT_LOCATION)
+      })
+
+    it("Updates the concert counts", async () => {
+        const totalConcerts = await tc.totalConcerts();
+        expect(totalConcerts).to.be.equal(1);
+    });
   });
 });
 
